10c8a0f7551e002b33136d3ed5df570e766fc520
MonicaDeBona <debona.monica@outlook.it>
2022-11-21 15:43:39 +0100

    Add js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const userAge = parseInt ( prompt("Type your age here"));
+
+const travelledKm = parseInt ( prompt("Type how many kilometers you want to travel"));
+
+const priceForKm =  21 / 100;
+
+const ticketPrice = travelledKm * priceForKm;
+
+const ticketMinorDiscount = (20 / 100 * ticketPrice);
+
+const ticketSeniorDiscount = (40 / 100 * ticketPrice);
+
+
+
+
+if (userAge < 18) {
+    document.getElementById ("output").innerHTML = ticketPrice.toFixed(2) - ticketMinorDiscount + "&euro;";
+} else if (userAge > 64) {
+    document.getElementById ("output").innerHTML = ticketPrice.toFixed(2) - ticketSeniorDiscount + "&euro;";
+} else {
+    document.getElementById ("output").innerHTML = ticketPrice.toFixed(2) + "&euro;";
+}
+
